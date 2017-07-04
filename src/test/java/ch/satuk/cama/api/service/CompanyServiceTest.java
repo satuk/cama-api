d5dbde46cfab9bed87dc99c947056f5d001588e6
Satuk Kolan <34@satuk.ch>
2017-07-04 08:40:16 +0200

    tested company service

---
+++ src/test/java/ch/satuk/cama/api/service/CompanyServiceTest.java
@@ -0,0 +1,48 @@
+package ch.satuk.cama.api.service;
+
+import ch.satuk.cama.api.entity.Company;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+
+/**
+ * Created by satuk on 03.07.17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = NONE)
+@Transactional
+@Sql("/test-data.sql")
+public class CompanyServiceTest {
+    
+    @Autowired
+    CompanyService service;
+    
+    @Test
+    public void findAll() {
+        assertThat( service.findAll().size() ).isEqualTo( 1 );
+    }
+    
+    @Test
+    public void findById() {
+        Company company = service.findById( 1L );
+        assertThat( company.getId() ).isEqualTo( 1 );
+    }
+    
+    @Test
+    public void findByIdIgnoreCaseConstaining() {
+        Company company = service.findById( 1L );
+        String companyNameToUpperCase = company.getName().toUpperCase();
+        
+        assertThat( service.findByIdIgnoreCaseConstaining( company.getName() ) ).isEqualTo( Arrays.asList( company ) );
+        assertThat( service.findByIdIgnoreCaseConstaining( company.getName().substring( 0, company.getName().length() / 2 ) ) ).isEqualTo( Arrays.asList( company ) );
+    }
+}
