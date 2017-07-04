cec8ae38c13eaf923e2f7cf5738d1173a34e8d1a
Satuk Kolan <34@satuk.ch>
2017-07-04 09:07:09 +0200

    removed unused annotation

--- src/main/java/ch/satuk/cama/api/repository/CompanyRepository.java
+++ src/main/java/ch/satuk/cama/api/repository/CompanyRepository.java
@@ -2,15 +2,12 @@ package ch.satuk.cama.api.repository;
 
 import ch.satuk.cama.api.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by satuk on 03.07.17.
  */
-
-@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     
     List<Company> findByNameIgnoreCaseContaining( String searchText );
