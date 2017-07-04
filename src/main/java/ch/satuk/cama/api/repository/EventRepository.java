9aa9c867954410788878e808fceb397d7d80fb15
Satuk Kolan <34@satuk.ch>
2017-07-04 12:31:58 +0200

    created event repository

---
+++ src/main/java/ch/satuk/cama/api/repository/EventRepository.java
@@ -0,0 +1,17 @@
+package ch.satuk.cama.api.repository;
+
+import ch.satuk.cama.api.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by satuk on 04.07.17.
+ */
+public interface EventRepository extends JpaRepository<Event, Long> {
+    
+    List<Event> findByEventNameIgnoreCaseContaining( String searchEvent );
+    
+    Event findById( Long id );
+    
+}
