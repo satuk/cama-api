475433893ae308ac0319fb84b49ccbe1c10dd7e9
Satuk Kolan <34@satuk.ch>
2017-07-04 10:45:10 +0200

    created user service interface

---
+++ src/main/java/ch/satuk/cama/api/service/UserService.java
@@ -0,0 +1,19 @@
+package ch.satuk.cama.api.service;
+
+import ch.satuk.cama.api.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by satuk on 04.07.17.
+ */
+
+public interface UserService {
+    
+    List<User> findAll();
+    
+    List<User> findByFirstNameIgnoreCaseContaining( String searchUser );
+    
+    User findById( Long id );
+}
