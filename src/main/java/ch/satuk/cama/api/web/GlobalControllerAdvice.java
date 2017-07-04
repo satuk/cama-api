9b1fdc4b62bcde1af84c9e163c10e5fe10e846fd
Satuk Kolan <34@satuk.ch>
2017-07-04 10:41:51 +0200

    added logger

--- src/main/java/ch/satuk/cama/api/web/GlobalControllerAdvice.java
+++ src/main/java/ch/satuk/cama/api/web/GlobalControllerAdvice.java
@@ -1,12 +1,14 @@
 package ch.satuk.cama.api.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by satuk on 04.07.17.
@@ -16,15 +18,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class GlobalControllerAdvice {
     
-    @InitBinder
-    public void configureBinding( WebDataBinder binder ) {
-        binder.setDisallowedFields( "id" );
-    }
+    private static final Logger logger = LoggerFactory.getLogger( GlobalControllerAdvice.class );
     
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public void handleDatabaseConstraintViolation() {
-        /* no-op */
+    public void handleDataIntegrityViolationException( DataIntegrityViolationException ex, HttpServletRequest request ) {
+        logger.error( String.format( "Data integrity violation for [%s] request to URL [%s].", request.getMethod(),
+                request.getRequestURL() ), ex );
     }
     
 }
