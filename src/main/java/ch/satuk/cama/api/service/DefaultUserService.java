ad5574a6885712aae81a2ae7729038c06abb6d8d
Satuk Kolan <34@satuk.ch>
2017-07-04 10:46:12 +0200

    implemented user service

---
+++ src/main/java/ch/satuk/cama/api/service/DefaultUserService.java
@@ -0,0 +1,45 @@
+package ch.satuk.cama.api.service;
+
+import ch.satuk.cama.api.entity.JsonViews;
+import ch.satuk.cama.api.entity.User;
+import ch.satuk.cama.api.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by satuk on 04.07.17.
+ */
+
+@Service
+@Transactional(readOnly = true)
+public class DefaultUserService implements UserService {
+    
+    @JsonView(JsonViews.Summary.class)
+    private final UserRepository repository;
+    
+    
+    @Autowired
+    public DefaultUserService( UserRepository repository ) {
+        this.repository = repository;
+    }
+    
+    
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    
+    @Override
+    public List<User> findByFirstNameIgnoreCaseContaining( String searchUser ) {
+        return repository.findByFirstNameIgnoreCaseContaining( searchUser );
+    }
+    
+    @Override
+    public User findById( Long id ) {
+        return repository.findById( id );
+    }
+}
