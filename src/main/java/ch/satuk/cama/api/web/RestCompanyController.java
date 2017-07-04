8fa58acbafc8bddd63a68f6ba10cca471926cbb4
Satuk Kolan <34@satuk.ch>
2017-07-04 10:41:28 +0200

    fixed controller

--- src/main/java/ch/satuk/cama/api/web/RestCompanyController.java
+++ src/main/java/ch/satuk/cama/api/web/RestCompanyController.java
@@ -3,12 +3,12 @@ package ch.satuk.cama.api.web;
 import ch.satuk.cama.api.entity.Company;
 import ch.satuk.cama.api.entity.JsonViews;
 import ch.satuk.cama.api.service.CompanyService;
-import ch.satuk.cama.api.service.DefaultCompanyService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,15 +16,14 @@ import java.util.List;
  * Created by satuk on 04.07.17.
  */
 
-@Controller
+@RestController
 @RequestMapping("/api/companies")
 public class RestCompanyController {
     
     private final CompanyService service;
     
-    
     @Autowired
-    public RestCompanyController( DefaultCompanyService service ) {
+    public RestCompanyController( CompanyService service ) {
         this.service = service;
     }
     
@@ -32,5 +31,11 @@ public class RestCompanyController {
     @GetMapping
     public List<Company> retrieveAllCompanies() {
         return service.findAll();
+    }
+    
+    @JsonView(JsonViews.Detail.class)
+    @GetMapping("/{id}")
+    public Company retrieveCompany( @PathVariable Long id ) {
+        return this.service.findById( id );
     }
 }
