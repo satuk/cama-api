f78dc23c906bd8b7a3afb937935f97c90d09e4f0
Satuk Kolan <34@satuk.ch>
2017-07-05 14:43:44 +0200

    added new api paths

--- src/main/java/ch/satuk/cama/api/web/RestUserController.java
+++ src/main/java/ch/satuk/cama/api/web/RestUserController.java
@@ -1,8 +1,12 @@
 package ch.satuk.cama.api.web;
 
+import ch.satuk.cama.api.entity.Application;
 import ch.satuk.cama.api.entity.JsonViews;
 import ch.satuk.cama.api.entity.User;
+import ch.satuk.cama.api.entity.Work;
+import ch.satuk.cama.api.service.ApplicationService;
 import ch.satuk.cama.api.service.UserService;
+import ch.satuk.cama.api.service.WorkService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,23 +24,38 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class RestUserController {
     
-    private final UserService service;
+    private final UserService userService;
+    private final WorkService workService;
+    private final ApplicationService applicationService;
     
     @Autowired
-    public RestUserController( UserService service ) {
-        this.service = service;
+    public RestUserController( UserService userService, WorkService workService, ApplicationService applicationService ) {
+        this.userService = userService;
+        this.workService = workService;
+        this.applicationService = applicationService;
     }
     
     @JsonView(JsonViews.Summary.class)
     @GetMapping
     public List<User> retrieveAllUsers() {
-        return this.service.findAll();
+        return this.userService.findAll();
     }
     
-    @JsonView(JsonViews.Summary.class)
+    @JsonView(JsonViews.Detail.class)
     @GetMapping("/{id}")
     public User retrieveUser( @PathVariable Long id ) {
-        return this.service.findById( id );
+        return this.userService.findById( id );
     }
     
+    @JsonView(JsonViews.Detail.class)
+    @GetMapping("/{id}/works")
+    public List<Work> retrieveUserAllWorks( @PathVariable Long id ) {
+        return this.workService.findWorksByUser_Id( id );
+    }
+    
+    @JsonView(JsonViews.Detail.class)
+    @GetMapping("/{id}/applications")
+    public List<Application> retrieveUserAllApplications( @PathVariable Long id ) {
+        return this.applicationService.findApplicationsByUser_Id( id );
+    }
 }
