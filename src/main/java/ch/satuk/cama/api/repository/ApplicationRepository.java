650363a8723cf6c33e7fcc40d8f18bd7da97b1eb
Satuk Kolan <34@satuk.ch>
2017-07-06 10:48:59 +0200

    added new methods

--- src/main/java/ch/satuk/cama/api/repository/ApplicationRepository.java
+++ src/main/java/ch/satuk/cama/api/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package ch.satuk.cama.api.repository;
 
 import ch.satuk.cama.api.entity.Application;
+import ch.satuk.cama.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,11 @@ import java.util.List;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     
+    Application findById( Long id );
+    
+    Application findApplicationByEvent_IdAndUser_Id( Long event_id, Long user_id );
+    
     List<Application> findApplicationsByUser_Id( Long id );
     
+    List<Application> findApplicationsByEvent_Id( Long id );
 }
