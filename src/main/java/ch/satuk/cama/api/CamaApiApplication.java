53e16a784d27b5cac0be6de52407b989cb855364
Satuk Kolan <34@satuk.ch>
2017-07-06 17:09:53 +0200

    added a corsConfigurer bean to the main

--- src/main/java/ch/satuk/cama/api/CamaApiApplication.java
+++ src/main/java/ch/satuk/cama/api/CamaApiApplication.java
@@ -2,11 +2,26 @@ package ch.satuk.cama.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CamaApiApplication {
     
     public static void main( String[] args ) {
         SpringApplication.run( CamaApiApplication.class, args );
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            
+            @Override
+            public void addCorsMappings( CorsRegistry registry ) {
+                registry.addMapping( "/**" );
+            }
+        };
     }
 }
