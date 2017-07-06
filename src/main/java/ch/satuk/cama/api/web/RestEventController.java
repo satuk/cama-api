23ce23afd2a536e48147d558505d0a40c058766b
Satuk Kolan <34@satuk.ch>
2017-07-06 14:17:28 +0200

    implemented retrieveAllAppliedUser and approveUser

--- src/main/java/ch/satuk/cama/api/web/RestEventController.java
+++ src/main/java/ch/satuk/cama/api/web/RestEventController.java
@@ -1,16 +1,17 @@
 package ch.satuk.cama.api.web;
 
-import ch.satuk.cama.api.entity.Event;
-import ch.satuk.cama.api.entity.JsonViews;
-import ch.satuk.cama.api.service.EventService;
+import ch.satuk.cama.api.entity.*;
+import ch.satuk.cama.api.service.*;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by satuk on 04.07.17.
@@ -20,23 +21,67 @@ import java.util.List;
 @RequestMapping("/api/events")
 public class RestEventController {
     
-    private final EventService service;
+    private final EventService eventService;
+    private final ApplicationService applicationService;
+    private final UserService userService;
+    private final WorkService workService;
+    
     
     @Autowired
-    public RestEventController( EventService service ) {
-        this.service = service;
+    public RestEventController( EventService eventService, ApplicationService applicationService, UserService
+            userService, WorkService workService ) {
+        this.eventService = eventService;
+        this.applicationService = applicationService;
+        this.userService = userService;
+        this.workService = workService;
     }
     
     
     @JsonView(JsonViews.Summary.class)
     @GetMapping
     public List<Event> retrieveAllEvents() {
-        return service.findAll();
+        return eventService.findAll();
     }
     
     @JsonView(JsonViews.Detail.class)
     @GetMapping("/{id}")
     public Event retrieveEvent( @PathVariable Long id ) {
-        return this.service.findById( id );
+        return this.eventService.findById( id );
     }
+    
+    @JsonView(JsonViews.Summary.class)
+    @GetMapping("/{id}/applications")
+    public List<User> retrieveAllAppliedUsers( @PathVariable Long id ) {
+        
+        return this.applicationService
+                .findApplicationsByEvent_Id( id )
+                .stream()
+                .map( application -> this.userService.findById( application.getUser().getId() ) )
+                .collect( Collectors.toList() );
+    }
+    
+    @JsonView(JsonViews.Summary.class)
+    @PostMapping("/{id}/applications")
+    public void approveUser( @RequestBody Map<String, String> json, @PathVariable Long id ) {
+        // approved user deleted from applied added working
+        // check if its in the applications list
+        
+        // find event and user
+        Event event = this.eventService.findById( id );
+        User user = this.userService.findById( Long.parseLong( json.get( "user_id" ) ) );
+        
+        // than save it and allow just once
+        this.workService.save( new Work( LocalDateTime.now(), event, user ) );
+        
+        // add the company to user's list
+        user.addCompany( event.getCompany() );
+//        this.userService.findById( user.getId() ).addCompany( event.getCompany() );
+        
+        // than delete it from applications table
+        Application application = this.applicationService.findApplicationByEvent_IdAndUser_Id( event.getId(), user
+                .getId() );
+        
+        this.applicationService.deleteById( application.getId() );
+    }
+    
 }
