04f5b15b30ff364aa8f63eeddc8b03100cb6cd6f
Satuk Kolan <34@satuk.ch>
2017-07-17 13:14:28 +0200

    update test-data with new properties

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -7,10 +7,10 @@ DELETE FROM users;
 
 INSERT INTO users (id, first_name, last_name, password, email, address, age, postal_code, handy_number, city, date_created) VALUES (1, 'Satuk', 'Kolan', 'password', 'satuk@kolan.ch', 'Im Brünneli 8', 33, 8127, 0793085018, 'Maur', {ts '2017-01-01 00:00:00.00'});
 
-INSERT INTO companies (id, name, address, postal_code, description, url, date_created) VALUES (1, 'Catering AG', '2526 cta 95th st, fort lauderdale, florida, united states, 33930', 5611, 'Lorem ipsum aenean suscipit ante tincidunt taciti, dictum mi fames inceptos vel natoque aenean?\nNulla in nullam lacus, auctor vel phasellus pulvinar senectus.\n', 'http://www.digg.com/books/howto/fr/fr/2013/09/404.html', {ts '2017-01-01'});
-INSERT INTO companies (id, name, address, postal_code, description, url, date_created) VALUES (2, 'Aura Group', '2526 cta 95th st, fort lauderdale, florida, united states, 33930', 5611, 'Lorem ipsum aenean suscipit ante tincidunt taciti, dictum mi fames inceptos vel natoque aenean?\nNulla in nullam lacus, auctor vel phasellus pulvinar senectus.\n', 'http://www.digg.com/books/howto/fr/fr/2013/09/404.html', {ts '2017-01-01'});
+INSERT INTO companies (id, name, address, city, postal_code, description, url, logo, public_events, date_created) VALUES (1, 'Aura', 'Bleicherweg 5', 'Zurich', 8001, 'AURA Restaurant, Bar & Smoker''s Lounge, Eventsaal', 'https://www.aura-zurich.ch/', 'https://www.aura-event.ch/wp-content/uploads/2015/09/footer75_aura1.png', true, {ts '2017-01-01'});
+INSERT INTO companies (id, name, address, city, postal_code, description, url, logo, public_events, date_created) VALUES (2, 'Metropol', 'Fraumunsterstrasse 12', 'Zurich', 8001, 'Metropol Restaurant & Café /Bar', 'http://metropol-restaurant.ch/', 'http://www.metropol-restaurant.ch/wp-content/uploads/2017/04/metropol_logo-300x72.png', true, {ts '2017-01-01'});
 
-INSERT INTO events (id, name, logo, date, start, end, different_location, expected_person, company_id, description, date_created, visibility) VALUES (1, 'Turkish Night', 'http://placehold.it/300x250', {ts '2017-10-10'}, {ts '2017-01-01 00:00:00.00'}, {ts '2017-01-01 00:00:00.00'}, 0, 400, 1, 'description' ,{ts '2017-01-01'}, true );
+INSERT INTO events (id, name, logo, date, start, end, different_location, expected_person, company_id, description, date_created, visibility) VALUES (1, 'PLANET 105 WE LOVE IBIZA BOAT', 'http://zuerich.usgang.ch/uploadfile/flyer/751920.jpg', {ts '2017-10-10'}, {ts '2017-01-01 18:00:00.00'}, {ts '2017-01-01 23:59:00.00'}, 0, 400, 1, 'DJs Tanja La Croix, Sam, Alex Price, Dino Supreme Show', {ts '2017-01-01'}, true );
 
 INSERT INTO users_companies(user_id, companies_id) VALUES (1, 1);
 
